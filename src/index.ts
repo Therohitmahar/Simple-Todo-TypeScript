5f34a04db7bf1bc1f5c7f3e3828772123c4c41ad
rohit mahar <therohitmahar@gmail.com>
2023-06-07 18:22:31 +0530

    first and final

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,63 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidv4 } from 'uuid';
+// console.log(uuidv4());
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const input = document.querySelector<HTMLInputElement>('#title');
+const form = document.querySelector('#form') as HTMLFormElement | null;
+const taskArr: Task[] = getFromLocal();
+taskArr?.forEach(addTodoTask);
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input?.value === '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  taskArr?.push(newTask);
+  saveToLocal();
+  addTodoTask(newTask);
+  input.value = '';
+});
+
+function addTodoTask(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveToLocal();
+  });
+
+  checkbox.type = 'checkbox';
+
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveToLocal() {
+  localStorage.setItem('TASKS', JSON.stringify(taskArr));
+}
+
+function getFromLocal(): Task[] {
+  try {
+    const jsonData = localStorage.getItem('TASKS');
+    if (jsonData == null) return [];
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return [];
+  }
+}
